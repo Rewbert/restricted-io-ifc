100bc9378c83661ab4b44ce30738167b6586537f
Robert Krook <krookr@chalmers.se>
2023-02-16 11:30:53 +0100

    add taint function

--- src/Internal/Untrusted.hs
+++ src/Internal/Untrusted.hs
@@ -1,10 +1,14 @@
 module Internal.Untrusted (
     Untrusted
+    , taint
     , trust
 ) where
 
 -- | A value that is untrusted, indicating that it came from an untrusted source
 data Untrusted a = Untrusted a
+
+taint :: a -> Untrusted a
+taint a = Untrusted a
 
 -- | Endorse an untrusted value
 trust :: Untrusted a -> a
